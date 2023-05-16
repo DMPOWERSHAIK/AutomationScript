ae632f49a21d97ebbb954ab03725e72c2ffce69d
MRehmaniShaik <MRehmaniShaik@DIST9J695Y2>
2023-05-16 14:31:06 -0400

    Add project files.

---
+++ dmpower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dmpower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("dmpower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6D178AD8-2672-49DA-BEE1-DE777B77F177")]
+[assembly: AssemblyVersion("1.0.0.0")]
